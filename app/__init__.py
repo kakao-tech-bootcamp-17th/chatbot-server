db1805062e4291ab1e8858dbb03d4ee04f5b18dc
gib.son <shai4956@gmail.com>
2024-08-23 01:43:38 +0900

    ♻️ 환경변수를 config 파일로 관리하도록 변경

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object('config.Config')
 
-    from app.local import local_bp
-    from app.weather import weather_bp
-
-    app.register_blueprint(local_bp, url_prefix='/local')
-    app.register_blueprint(weather_bp, url_prefix='/weather')
+    
+    with app.app_context():
+        # 애플리케이션 컨텍스트 내에서 초기화 코드 실행
+        from app.local import local_bp
+        from app.weather import weather_bp
+        app.register_blueprint(local_bp, url_prefix='/local')
+        app.register_blueprint(weather_bp, url_prefix='/weather')
 
     return app
