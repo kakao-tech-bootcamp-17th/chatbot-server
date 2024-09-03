68bb777d4ac92a8a727803447489d116ceff4b7f
JOKER_SON <alreadysons@gmail.com>
2024-09-03 14:38:07 +0900

    🚚 weather_route.py를 route 디렉토리로 이동

---
+++ app/weather/route/weather_route.py
@@ -0,0 +1,23 @@
+from flask import request, jsonify
+from app.weather.service.weather_service import WeatherService
+from .. import weather_bp
+from app.exception.bad_reqeust_exception import BadRequestException
+
+weather_service = WeatherService()
+
+@weather_bp.route("/coordinate", methods=['GET']) #TODO:임시로 사용하고 추후 제거할것 
+def get_weather_by_coordinate():
+    lat = request.args.get("lat")
+    lon = request.args.get("lon")
+    if not lat or not lon:
+        raise BadRequestException("위도와 경도는 필수 값입니다.")
+    
+    response = weather_service.get_weather_by_coordinate(lat, lon)
+    return jsonify(response)
+@weather_bp.route("/", methods=['GET'])   #일반적으로 사용될 것
+def get_weather_by_address():
+    address = request.args.get("address")
+    if not address:
+        raise BadRequestException("주소지는 필수 값입니다.")
+    response = weather_service.get_weather_by_address(address)
+    return jsonify(response)
