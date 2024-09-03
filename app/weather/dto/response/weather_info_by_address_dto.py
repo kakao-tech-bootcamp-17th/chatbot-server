77948cf7f520aaa70f840b53aa3ba9fcbe4f3bda
JOKER_SON <alreadysons@gmail.com>
2024-09-03 16:59:10 +0900

    ⚰️ 불필요한 띄워쓰기 줄 제거

--- app/weather/dto/response/weather_info_by_address_dto.py
+++ app/weather/dto/response/weather_info_by_address_dto.py
@@ -1,8 +1,7 @@
-# dto/response/weather_info_by_address_dto.py
 from __future__ import annotations
 from dataclasses import dataclass
-from ....local.dto.response.place_coordinate_response_dto import PlaceCoordinateResponseDto
-from .weather_info_response_dto import WeatherInfoResponseDto
+from app.local.dto.response.place_coordinate_response_dto import PlaceCoordinateResponseDto
+from app.weather.dto.response.weather_info_response_dto import WeatherInfoResponseDto
 
 @dataclass(frozen=True)
 class WeatherInfoByAddressDto:
@@ -13,7 +12,6 @@ class WeatherInfoByAddressDto:
     @classmethod
     def from_data(cls, address: str, coordinates: dict, weather: WeatherInfoResponseDto) -> WeatherInfoByAddressDto:
         coordinate_dto = PlaceCoordinateResponseDto.from_data(coordinates)
-        print("cls",cls)
         return cls(
             address=address,
             coordinates=coordinate_dto,
