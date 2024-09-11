afe56d162f3f842856ea9f42854c6ce3e30178c2
“sanghoon416” <jk08097@naver.com>
2024-09-11 15:43:59 +0900

    기능 최종

--- app/gpt/chat_gpt_api.py
+++ app/gpt/chat_gpt_api.py
@@ -4,6 +4,7 @@ from . import gpt_bp
 import json
 from app.weather.service.weather_service import WeatherService
 from app.traffic.service.traffic_service import TrafficService
+from app.local.service.local_service import LocalService
 
 # Debugging Package
 # import time
@@ -12,13 +13,15 @@ from datetime import datetime
 
 # 채팅 모델 컨텍스트
 CHAT_MODEL_CONTEXT = '''
-    당신은 사용자 발화로부터 지역명을 추출하고, 어떤 정보가 필요한지에 따라 카테고리를 분류합니다.
+    당신은 사용자 발화로부터 지역명, 키워드를 추출하고, 어떤 정보가 필요한지에 따라 카테고리를 분류합니다.
     지역명은 1개만 있거나, 출발지와 도착지 정보 2개가 있는 경우로 나뉩니다.
-    사용자가 원하는 정보가 '날씨','교통','맛집' 중에 어떤 카테고리인지 파악합니다.
+    사용자가 원하는 정보가 '날씨','교통','지도' 중에 어떤 카테고리인지 파악합니다.
+    키워드는 카테고리가 '지도' 인 경우 지도에서 검색할 키워드 (예 맛집, 카페, 중국집, 주차장 등등)
     json 형식으로 출력합니다.
     1. 지역명 (1개인 경우)
     2. 출발지와 도착지 지역명 (2개인 경우)
     3. 카테고리
+    4. 키워드 (카테고리가 '지도'인 경우)
     '''
     
 # OpenAI API 클라이언트 설정
@@ -49,10 +52,10 @@ def chat_response():
  
     pre_response_data = json.loads(pre_response_message.content)
     category = pre_response_data.get("카테고리")
+    response = ""
     
     # 교통 프롬프트 후처리 
     if category == "교통" :
-        
         start = pre_response_data.get("출발지")
         goal = pre_response_data.get("도착지")
 
@@ -75,7 +78,7 @@ def chat_response():
             messages=messageList
         )
         post_response_message = post_completion.choices[0].message
-        
+
         response = {
             "version": "2.0",
             "template": {
@@ -88,14 +91,10 @@ def chat_response():
                 ]
             }
         }
-
-        writeLog_debug(response)
-
         return jsonify(response)
 
     # 날씨 프롬프트 후처리
-    if category == "날씨"
-        category = pre_response_data.get("카테고리")
+    if category == "날씨" :
         city = pre_response_data.get("지역명")
 
         weather_service = WeatherService()
@@ -115,10 +114,8 @@ def chat_response():
         날씨 : {description}
 
         위의 정보를 이용해서
-        {utterance}에 대한 대답을 해줘
+        {utterance}에 대해서 아나운서처럼 대답을 해줘
         '''
-        
-        print(weather_prompt)
         
         messageList = [{"role": "system", "content": weather_prompt}]
         dicMessage = {"role": "user", "content": utterance}
@@ -129,7 +126,7 @@ def chat_response():
             messages=messageList
         )
         post_response_message = post_completion.choices[0].message
-        
+
         response = {
             "version": "2.0",
             "template": {
@@ -142,18 +139,41 @@ def chat_response():
                 ]
             }
         }
-
-        writeLog_debug(response)
-
         return jsonify(response)
+        
+    if category == "지도" :
+        location = pre_response_data.get("지역명")
+        keyword = pre_response_data.get("키워드")
+        map_service = LocalService()
+        map_info = map_service.search_places(location, keyword)
 
+        prompt = f'''
+        {map_info}를 참고해서
+        가장 추천하는 장소의 거리와 이름을 2곳 알려줘
+        '''
 
-def writeLog_debug(response) : 
-    # print("테스트용 확인 TimeSleep ------------------------")
-    # time.sleep(2)
-    now = datetime.now().strftime('%Y.%m.%d - %H:%M:%S')
-    txtFileName = datetime.now().strftime('%Y%m%d%H%M%S')
-    log = f"{now} -- \nresponse : {response}"
-    with open(f"./app/log/20240910.txt", 'a') as file :
-        file.write("\n\n" +log)
-        file.close()
+        messageList = [{"role": "system", "content": prompt}]
+        dicMessage = {"role": "user", "content": utterance}
+        messageList.append(dicMessage)
+
+        post_completion = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=messageList
+        )
+        post_response_message = post_completion.choices[0].message
+
+        response = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "simpleText": {
+                            "text": f"{post_response_message.content}"
+                        }
+                    }
+                ]
+            }
+        }
+        return jsonify(response)
+    
+    return jsonify(response)
